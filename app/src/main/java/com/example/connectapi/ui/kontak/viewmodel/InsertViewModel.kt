34932aade55997a58f61a97a6c62c1a26b657e18
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-27 12:17:20 +0700

    add kontak to uistate kontak dan to insert Ui Event

--- app/src/main/java/com/example/connectapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/connectapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -47,4 +47,14 @@ fun InsertUiEvent.toKontak(): Kontak = Kontak(
     telpon = telepon,
 )
 
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
+)
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = email,
+    telepon = telpon,
+)
+
 
